64e970abfd31c923bcdf0241b4aa5bc95bbbbfe5
alexadusei <alex.adusei@gmail.com>
2018-07-16 19:17:40 -0400

    Add third party middleware for routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import client from 'redis'
+import compression from 'compression'
 import express from 'express'
+import helmet from 'helmet'
+import limiter from 'express-limiter'
 import mongoose from 'mongoose'
 import winston from 'winston'
 
@@ -11,15 +15,34 @@ import textbooks from './api/textbooks'
 
 const test = process.argv.join().match('/ava/')
 const URI = process.env.QMULUS_MONGO_URI || 'mongodb://localhost:27017/qmulus'
+const redisClient = client.createClient()
+const version = 'v1'
+
+const app = express()
+const rateLimiter = limiter(app, redisClient)
 
 mongoose.connect(URI, { useNewUrlParser: true }, err => {
   if (err) throw new Error('Connection failed')
   if (!test) winston.info('Connected to database')
 })
 
-const app = express()
-const version = 'v1'
+// API rate limiting
+rateLimiter({
+  path: '*',
+  method: 'get',
+  lookup: ['connection.remoteAddress'],
+  total: 1000, // 1000 requests
+  expire: 1000 * 60 * 60, // per hour
+  onRateLimited: (req, res, next) => {
+    next({ status: 429, message: 'API rate limit exceeded' })
+  },
+})
 
+// Third-party middleware
+app.use(helmet())
+app.use(compression())
+
+// API routes
 app.use(`/${version}/buildings`, buildings)
 app.use(`/${version}/courses`, courses)
 app.use(`/${version}/departments`, departments)
@@ -27,6 +50,7 @@ app.use(`/${version}/news`, news)
 app.use(`/${version}/sections`, sections)
 app.use(`/${version}/textbooks`, textbooks)
 
+// Error handlers
 app.use((req, res, next) => {
   const err = new Error('Not Found')
   err.status = 404
@@ -40,9 +64,5 @@ app.use((err, req, res, next) => {
 
   res.json({ error: error })
 })
-
-// app.listen(3000, () => {
-//   winston.info(`Server running at http://localhost:3000`)
-// })
 
 export default { Server: app }
