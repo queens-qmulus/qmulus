3ff307bef84f2496d5498b366ed4f1f7421c5804
Enoch Tam <et.enoch@gmail.com>
2019-11-17 21:48:51 +0000

    Add datadog logging transport.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,4 +1,14 @@
 import winston from 'winston'
+import DatadogTransport from '@shelf/winston-datadog-logs-transport'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const DD_MANUAL_TRANSPORT = process.env.DD_MANUAL_TRANSPORT === 'true'
+const DD_API_KEY = process.env.DD_API_KEY
+const LOGGING_SOURCE = process.env.LOGGING_SOURCE
+const LOGGING_ENVIRONMENT = process.env.LOGGING_ENVIRONMENT
+const LOGGING_HOST = process.env.LOGGING_HOST
+const LOGGING_SERVICE = process.env.LOGGING_SERVICE
 
 const customLevels = {
   levels: {
@@ -27,9 +37,25 @@ const consoleTransport = new winston.transports.Console({
   ),
 })
 
+const transports = [consoleTransport]
+
+if (DD_MANUAL_TRANSPORT) {
+  const ddTransport = new DatadogTransport({
+    apiKey: DD_API_KEY,
+    format: winston.format.json(),
+    metadata: {
+      ddsource: LOGGING_SOURCE,
+      environment: LOGGING_ENVIRONMENT,
+      host: LOGGING_HOST,
+      service: LOGGING_SERVICE,
+    },
+  })
+  transports.push(ddTransport)
+}
+
 const logger = winston.createLogger({
   levels: customLevels.levels,
-  transports: [consoleTransport],
+  transports,
 })
 
 winston.addColors(customLevels)
@@ -38,7 +64,39 @@ winston.addColors(customLevels)
 const format = ':remote-addr - :remote-user ":method :url HTTP/:http-version"' +
 ' :status :res[content-length] ":referrer" ":user-agent"'
 
-logger.morganFormat = process.env.NODE_ENV === 'production' ? format : 'dev'
+const jsonFormat = (tokens, req, res) => {
+  const url = tokens.url(req, res)
+  const path = url.split('?')[0]
+  const queryString = url.split('?')[1]
+  const params = new URLSearchParams(queryString)
+  const paramsObj = {}
+  for (const [key, value] of params) {
+    paramsObj[key] = value
+  }
+
+  return JSON.stringify({
+    network: {
+      // TODO: content-length not actually being written
+      bytes_written: tokens.res(req, res, 'content-length'),
+      client: {
+        ip: tokens['remote-addr'](req, res),
+      },
+    },
+    duration: parseInt(tokens['response-time'](req, res)) * 1000000,
+    http: {
+      method: tokens.method(req, res),
+      url,
+      status_code: tokens.status(req, res),
+      url_details: {
+        path,
+        queryString: paramsObj,
+      },
+    },
+  })
+}
+
+const prodFormat = DD_MANUAL_TRANSPORT ? jsonFormat : format
+logger.morganFormat = process.env.NODE_ENV === 'production' ? prodFormat : 'dev'
 logger.winstonStream = {
   write: (message, encoding) => {
     // remove Morgan's newline
